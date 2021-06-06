18b8f8aeb8546016a034954e1f172b71e7b96842
qazybi <k.askarbek@innopolis.university>
2021-06-06 22:38:31 +0300

    resnet34 + U-net

--- semantic-segmentation/model.py
+++ semantic-segmentation/model.py
@@ -3,97 +3,66 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def conv_bn_relu(in_planes, out_planes, kernel=3, stride=1, padding=1):
-    net = nn.Sequential(nn.Conv2d(in_planes, out_planes, 
-                                  kernel_size=kernel, stride=stride, padding=1),
-                        nn.BatchNorm2d(num_features=out_planes),
-                        nn.ReLU(inplace=True))
-    return net
-
-
-class Stacked2ConvsBlock(nn.Module):
-    def __init__(self, in_planes, out_planes):
-        super(Stacked2ConvsBlock, self).__init__()
-        self.blocks = nn.Sequential(conv_bn_relu(in_planes, out_planes), 
-                                    conv_bn_relu(out_planes, out_planes))
-
-    def forward(self, net):
-        net = self.blocks(net)
-        return net
-
-    
-
 class UpSamplingBlock(nn.Module):
-    def __init__(self, in_planes, out_planes):
-        super(UpSamplingBlock, self).__init__()
-        # понижаем число каналов
-        self.upsample = nn.ConvTranspose2d(in_planes, in_planes, 
-                                           kernel_size=2, stride=2)
-        # а потом стакаем с симметричным слоем из левой половины "параболы"
-        # ясно, что число каналов входной карты при этом удваивается
-        self.convolve = Stacked2ConvsBlock(2 * in_planes, out_planes)
+    def __init__(self, up_in, x_in, n_out):
+        super().__init__()
+        up_out = x_out = n_out // 2
+        self.x_conv = nn.Conv2d(x_in, x_out, 1)
+        self.tr_conv = nn.ConvTranspose2d(up_in, up_out, 2, stride=2)
+        self.bn = nn.BatchNorm2d(n_out)
 
-    def forward(self, left_net, right_net):
-        # нужно, чтобы состыковать число каналов
-        right_net = self.upsample(right_net)
-        net = torch.cat([left_net, right_net], dim=1)
-        net = self.convolve(net)
-        return net
+    def forward(self, up_p, x_p):
+        up_p = self.tr_conv(up_p)  # up path
+        x_p = self.x_conv(x_p)  # cross path
+        cat_p = torch.cat([up_p, x_p], dim=1)  # dim 1 means concat from means left to right
+        return self.bn(F.relu(cat_p))
 
-    
-class DownSamplingBlock(nn.Module):
-    def __init__(self, in_planes, out_planes):
-        super(DownSamplingBlock, self).__init__()
-        self.blocks = nn.Sequential(nn.MaxPool2d(2, 2),
-                                    Stacked2ConvsBlock(in_planes, out_planes))
 
-    def forward(self, net):
-        net = self.blocks(net)
-        return net
+# in order to pass output of resnet layers to upsampling blocks
+class SaveFeatures:
+    features = None
+
+    def __init__(self, m):
+        self.hook = m.register_forward_hook(self.hook_fn)
+
+    def hook_fn(self, module, inp, out):
+        self.features = out
+
+    def remove(self):
+        self.hook.remove()
+
+
+# inspired by Jeremy Howards idea on using resnet in U-net
+def get_base():
+    f = torch.hub.load('pytorch/vision:v0.9.0', 'resnet34', pretrained=True)
+    base_model = nn.Sequential(*list(f.children())[:-2])
+    return base_model
 
 
 class SegmenterModel(nn.Module):
     def __init__(self):
         super(SegmenterModel, self).__init__()
-        self.init_ch = 64 # число каналов после первой свёртки
-        self.n_levels = 3 # число уровней до "основания" параболы
-        self.init_conv = Stacked2ConvsBlock(3, 64)
 
-        self.downsample_1 = DownSamplingBlock(64, 128)
-        self.downsample_2 = DownSamplingBlock(128, 256)
-        self.downsample_3 = DownSamplingBlock(256, 512)
-        self.downsample_4 = DownSamplingBlock(512, 1024)
-         
-        # в середине есть блок без пары с 1024 каналами
-        # с ним конкатенировать некого, потому просто свернём его        
-        self.upconv = Stacked2ConvsBlock(self.init_ch * 16, 
-                                         self.init_ch * 8)
-        
-        # Подъём. Аналогично.
+        self.resnet = get_base()
+        self.sfs = [SaveFeatures(self.resnet[i]) for i in [2, 4, 5, 6]]
 
-        self.upsample_1 = UpSamplingBlock(512, 256)
-        self.upsample_2 = UpSamplingBlock(256, 128)
-        self.upsample_3 = UpSamplingBlock(128, 64)
-        # чтобы учесть входной слой после самой первой свёртки и согласовать размерности
-        self.upsample_4 = UpSamplingBlock(64, 64)
-        
-        # предсказание
-        self.agg_conv = nn.Conv2d(64, 1, kernel_size=1)
+        self.up1 = UpSamplingBlock(512, 256, 256)
+        self.up2 = UpSamplingBlock(256, 128, 256)
+        self.up3 = UpSamplingBlock(256, 64, 256)
+        self.up4 = UpSamplingBlock(256, 64, 256)
+        self.up5 = nn.ConvTranspose2d(256, 1, 2, stride=2)
 
     def forward(self, x):
-        net0 = self.init_conv(x) # 3 --> 64
-       
-        net1 = self.downsample_1(net0) # 64 --> 128
-        net2 = self.downsample_2(net1) # 128 --> 256
-        net3 = self.downsample_3(net2) # 256 --> 512
-        net = self.downsample_4(net3) # 512 --> 1024
-        
-        net = self.upconv(net) # 1024 --> 512
-        
-        net = self.upsample_1(net3, net) # 512 --> 256
-        net = self.upsample_2(net2, net) # 256 --> 128
-        net = self.upsample_3(net1, net) # 128 --> 64
-        net = self.upsample_4(net0, net) # 64 --> 64
-        
-        net = self.agg_conv(net) # 64 --> 1
-        return net
+        # resnet34 as downsampling and bottleneck
+        x = F.relu(self.resnet(x))
+        # upsampling
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x)
+        return x
+
+    def close(self):
+        for sf in self.sfs:
+            sf.remove()
--- semantic-segmentation/train.py
+++ semantic-segmentation/train.py
@@ -1,9 +1,9 @@
 import argparse
 import os
 import shutil
-
+import math
 import torch
-import torch.nn as nn
+# import torch.nn as nn
 import torch.utils.data as dt
 
 from carvana_dataset import CarvanaDataset
@@ -34,6 +34,23 @@ DEVICE_ID = 0
 DEVICE = torch.device(f"cuda:{DEVICE_ID}")
 torch.cuda.set_device(DEVICE_ID)
 
+def cyclical_lr(stepsize, min_lr=3e-4, max_lr=3e-3):
+
+    # Scaler: we can adapt this if we do not want the triangular CLR
+    scaler = lambda x: 1.
+
+    # Lambda function to calculate the LR
+    def lr_lambda(it):
+        return min_lr + (max_lr - min_lr) * relative(it, stepsize)
+
+    # Additional function to see where on the cycle we are
+    def relative(it, stepsize):
+        cycle = math.floor(1 + it / (2 * stepsize))
+        x = abs(it / stepsize - 2 * cycle + 1)
+        return max(0, (1 - x)) * scaler(cycle)
+
+    return lr_lambda
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -46,7 +63,7 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    
+
     model = SegmenterModel().to(DEVICE)  # Модель
     criterion = torch.nn.BCEWithLogitsLoss().to(DEVICE)  # Лосс
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)  # Алгоритм оптимизации
@@ -55,32 +72,47 @@ def main():
     ds_test = CarvanaDataset(TEST_DIR, TEST_MASKS_DIR)  # Тестовая выборка
 
     # Инструменты для подгрузки тензоров с данными
-    dl      = dt.DataLoader(ds, shuffle=True, 
-                            num_workers=8, 
+    dl      = dt.DataLoader(ds, shuffle=True,
+                            num_workers=2,
                             batch_size=args.batch_size)
-    dl_test = dt.DataLoader(ds_test, shuffle=False, 
-                            num_workers=8,
+    dl_test = dt.DataLoader(ds_test, shuffle=False,
+                            num_workers=2,
                             batch_size=args.batch_size)
+
+    step_size = 4 * len(dl)
+    clr = cyclical_lr(step_size)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, [clr])
+
+    ct = 0
+    for child in model.children():
+        if ct < 3:
+            for param in child.parameters():
+                param.requires_grad = False
+        ct += 1
 
     global_iter = 0
     for epoch in range(0, args.n_epochs):
-        print ("Current epoch: ", epoch)
+        print("Current epoch: ", epoch)
         epoch_loss = 0
         model.train(True)
         for i, (input_batch, target_batch) in enumerate(tqdm(dl)):
+            optimizer.zero_grad()
+
             input_batch = Variable(input_batch).cuda()
             target_batch = Variable(target_batch).cuda()
             output_batch = model(input_batch)
+
             loss = criterion(output_batch, target_batch)
             loss.backward()
+
             optimizer.step()
             global_iter += 1
             epoch_loss += loss.item()
         epoch_loss = epoch_loss / float(len(ds))
-        print ("Epoch loss", epoch_loss)
+        print("Epoch loss", epoch_loss)
         tb_writer.add_scalar('Loss/Train', epoch_loss, epoch)
 
-        print ("Make test")
+        print("Make test")
         test_loss = 0
         model.train(False)
         tb_out = np.random.choice(range(0, len(dl_test)), 3)
@@ -94,22 +126,28 @@ def main():
 
             for img_id, checkpoint in enumerate(tb_out):
                 if checkpoint == i:
-                    tb_writer.add_image(f'Image/Test_input_{img_id}',  
-                                        input_batch[0].cpu(), 
+                    tb_writer.add_image(f'Image/Test_input_{img_id}',
+                                        input_batch[0].cpu(),
                                         epoch)
-                    tb_writer.add_image(f'Image/Test_target_{img_id}', 
-                                        target_batch[0].cpu(), 
+                    tb_writer.add_image(f'Image/Test_target_{img_id}',
+                                        target_batch[0].cpu(),
                                         epoch)
-                    tb_writer.add_image(f'Image/Test_output_{img_id}', 
-                                        output_batch[0].cpu() > 0, 
+                    tb_writer.add_image(f'Image/Test_output_{img_id}',
+                                        output_batch[0].cpu() > 0,
                                         epoch)
 
         test_loss = test_loss / float(len(ds_test))
-        print ("Test loss", test_loss)
+        print("Test loss", test_loss)
         tb_writer.add_scalar('Loss/Test', test_loss, epoch)
         torch.save(model.state_dict(), "unet_dump_recent")
+
+        try:
+            torch.save(model.state_dict(), "drive/MyDrive/hw6_sber/unet_dump_recent")
+        except:
+            pass
+
+        scheduler.step()
 
 
 if __name__ == '__main__':
     main()
-    
